344af721dcf0817657b7c3f79fd2205e89ccde6a
dhalima3 <dhalima@gatech.edu>
2015-11-25 15:04:12 -0500

    add date to output

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import requests, bs4
+import requests, bs4, smtplib
 
 def main():
     res = requests.get('http://www.rotoworld.com/')
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     reports = []
     impacts = []
+    dates = []
     final = []
+    for date in soup.findAll(attrs={'class' : 'date'}):
+        # to get rid of dates we don't care about
+        if '-' in date.contents[0]:
+            dates.append(date.contents[0])
     for report in soup.findAll(attrs={'class' : 'report'}):
         reports.append(report.contents[1].getText())
     for impact in soup.findAll(attrs={'class' : 'impact'}):
@@ -16,10 +21,10 @@ def main():
     for i in range(len(reports)):
         final.append("REPORT:")
         final.append(reports[i])
-        final.append("\n")
         final.append("IMPACT:")
         final.append(impacts[i])
-
+        final.append("DATE:")
+        final.append(dates[i])
 
 
 main()
