b45694227fba0a7aacba3d2198cbde769dec34a4
dhalima3 <dhalima@gatech.edu>
2015-11-25 01:52:18 -0500

    format player info

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ def main():
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     reports = []
     impacts = []
+    final = []
     for report in soup.findAll(attrs={'class' : 'report'}):
         reports.append(report.contents[1].getText())
     for impact in soup.findAll(attrs={'class' : 'impact'}):
         impacts.append(impact.contents[0].strip())
+
+    # roto stuff on the first entry that we don't need
+    impacts.pop(0)
+    for i in range(len(reports)):
+        final.append("REPORT:")
+        final.append(reports[i])
+        final.append("\n")
+        final.append("IMPACT:")
+        final.append(impacts[i])
+
 
 
 main()
