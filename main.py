032c80ec5b6fda046481663dd4a9803c4ded644f
dhalima3 <dhalima@gatech.edu>
2015-12-02 16:36:27 -0500

    use MIMEText so line breaks would be sent in email

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 import bs4
 import smtplib
+from email.mime.text import MIMEText
 import datetime
 from twilio.rest import TwilioRestClient
 from secrets import *
 
+email_subject = "Fantasy Football Alerts"
+url = "http://www.rotoworld.com/"
 
 def main():
-    res = requests.get('http://www.rotoworld.com/')
+    res = requests.get(url)
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     reports = []
     impacts = []
@@ -25,7 +28,8 @@ def main():
     # roto stuff on the first entry that we don't need
     impacts.pop(0)
     updates = get_updates(reports, impacts, dates)
-    final = format_email(updates)
+    updates = format_updates(updates)
+    final = make_message(updates)
 
     print final
     if final:
@@ -40,7 +44,7 @@ def get_updates(reports, impacts, dates):
         time_split = map(int, date_split[3].split(":"))
         am_or_pm = date_split[4]
         hour = time_split[0] + 12 if (am_or_pm == "PM") else time_split[0]
-        # minute = time_split[1]
+        minute = time_split[1]
         if (current_time.hour - hour == 0) and (current_time.minute - minute <= 7):
             updates.append("REPORT: " + reports[i])
             updates.append("IMPACT: " + impacts[i])
@@ -48,25 +52,29 @@ def get_updates(reports, impacts, dates):
     return updates
 
 
-def format_email(updates):
+def format_updates(updates):
     final = ""
-    #final = "From: " + email_username + "<" +  email_username + ">\n"
-    #final += "To: To Person <" + destination_email + ">\n"
-    #final += "Subject: Fantasy Football Alert\n"
     updates = map(str, updates)
     for i in range(len(updates)):
         final += updates[i] + '\n'
         if ((i+1) % 3 == 0):
-            final += '====================================================\n'
+            final += '\n'
     return final
 
+
+def make_message(updates):
+    msg = MIMEText(updates)
+    msg['Subject'] = email_subject
+    msg['From'] = email_username
+    msg['To'] = destination_email
+    return msg
 
 def send_email(updates):
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.ehlo()
     server.starttls()
     server.login(email_username, email_password)
-    server.sendmail(email_username, destination_email, updates)
+    server.sendmail(email_username, destination_email, updates.as_string())
     server.quit()
 
 
